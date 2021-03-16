b2b73dfca226645b9d5713b480616bfb8fb9352a
Christopher J. Dajer <chrisdajer@gmail.com>
2021-03-16 13:24:12 -0400

    commit tree ba5eaf38637146f5ecfb93628655fb7c565527df

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function saturdayFun(target = "roller-skate") {
+    console.log(`This Saturday, I want to ${target}!`);
+}
+saturdayFun();
+
+function mondayWork(target = "I go to the office") {
+    console.log(`This Monday, I will ${target}`);
+}
+mondayWork();
